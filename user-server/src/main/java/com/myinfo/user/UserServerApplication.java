fce5ccce66baf15a391643d2f4bf7785434ac8b6
盛凯 <shengk@he-yue.com.cn>
2021-02-25 17:45:25 +0800

    优化

--- user-server/src/main/java/com/myinfo/user/UserServerApplication.java
+++ user-server/src/main/java/com/myinfo/user/UserServerApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @SpringBootApplication
 @MapperScan({"com.myinfo.base.mapper.*"})
-@ComponentScan("com.myinfo")
 @EnableJpaRepositories(basePackages = "com.myinfo.base.dao")
 @EntityScan(basePackages = "com.myinfo.base.entity")
 public class UserServerApplication {
