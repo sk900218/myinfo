d300565c49e13b7708870be8a416733ac5cd69ce
盛凯 <shengk@he-yue.com.cn>
2021-02-26 11:44:45 +0800

    路由搞定

--- user-server/src/main/java/com/myinfo/user/UserServerApplication.java
+++ user-server/src/main/java/com/myinfo/user/UserServerApplication.java
@@ -4,10 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableEurekaClient
 @SpringBootApplication
 @MapperScan({"com.myinfo.base.mapper.*"})
 @EnableJpaRepositories(basePackages = "com.myinfo.base.dao")
--- zuul-server/src/main/java/com/myinfo/zuul/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/myinfo/zuul/ZuulServerApplication.java
@@ -2,16 +2,13 @@ package com.myinfo.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 /**
  * zuul实现路由功能以及与ribbon共同实现负载均衡
  * 在其入口applicaton类加上注解@EnableZuulProxy，开启zuul的功能
  */
 @EnableZuulProxy
-@EnableEurekaClient
 @SpringBootApplication
 public class ZuulServerApplication {
 
