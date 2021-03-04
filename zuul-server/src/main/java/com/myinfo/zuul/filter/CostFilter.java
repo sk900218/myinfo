d384e8544302f382c17895363837fe78ef6e5359
盛凯 <shengk@he-yue.com.cn>
2021-03-04 17:45:21 +0800

    跨域问题

--- zuul-server/src/main/java/com/myinfo/zuul/filter/CostFilter.java
+++ zuul-server/src/main/java/com/myinfo/zuul/filter/CostFilter.java
@@ -18,14 +18,18 @@ public class CostFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
 
-        HttpServletRequest reqs = (HttpServletRequest) req;
+        HttpServletRequest request = (HttpServletRequest) req;
 
         // response.setHeader("Access-Control-Allow-Origin",reqs.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-AUTH-TOKEN");
+        if(request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
         chain.doFilter(req, res);
     }
 
